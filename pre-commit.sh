d1d665f554500aa5fd1e0e85cf154e2248775a14
Ilya Rusyanov <ilya.rusyanov@gmail.com>
2019-09-04 11:32:22 +0300

    do not stash before and stash apply after commit
    i do not like it anymore

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,9 +2,6 @@
 
 # https://codeinthehole.com/tips/tips-for-using-a-git-pre-commit-hook/
 
-STASH_NAME="pre-commit-$(date +%s)"
-git stash save -q --keep-index $STASH_NAME
-
 # check for ADDED lines with tabs
 tabchar=$(printf '\t')
 for file in $(git diff --name-status --cached | grep -v ^D | cut -c3-); do
@@ -63,10 +60,4 @@ if [[ $(basename $(pwd)) == "${COMMON_FILES}" ]]; then
             exit 1
         fi
     fi
-fi
-
-STASHES=$(git stash list)
-
-if [[ $STASHES == "$STASH_NAME" ]]; then
-  git stash pop -q
 fi
